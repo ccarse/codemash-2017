220395668ffe26b6b55cc7f7b6e1597939dfee9f
Cody Carse <ccarse@gmail.com>
2017-01-14 16:38:07 -0500

    Yay circles!

---
+++ src/getBuses.js
@@ -0,0 +1,45 @@
+var GtfsRealtimeBindings = require('gtfs-realtime-bindings');
+var request = require('request');
+var turf = require('turf');
+var fs = require('fs');
+
+var requestSettings = {
+  method: 'GET',
+  url: 'http://realtime.cota.com/TMGTFSRealTimeWebService/Vehicle/VehiclePositions.pb',
+  encoding: null
+};
+
+function getBuses() {
+  var buses = [];
+  return new Promise((resolve, reject) => {
+    request(requestSettings, function (error, response, body) {
+      if (!error && response.statusCode == 200) {
+        var feed = GtfsRealtimeBindings.FeedMessage.decode(body);
+        for (entity of feed.entity) {
+          console.log(entity);
+          if (entity.vehicle) {
+            buses.push({
+              "id": entity.id,
+              "location": { 
+                "bearing": entity.vehicle.position.bearing,
+                "point": turf.point([entity.vehicle.position.longitude, entity.vehicle.position.latitude])
+              }
+            });
+          }
+        }
+        resolve(buses);
+      } else {
+        reject(error + response.statusCode);
+      }
+    });
+  });
+}
+
+function writeBuses() {
+  getBuses().then(buses => { 
+    //console.log(JSON.stringify(buses, null, 4));
+    fs.writeFileSync("./data/buses.json", JSON.stringify(buses));  
+  }).catch(err => console.log(err));
+}
+
+writeBuses();
--- src/index.js
+++ src/index.js
@@ -1,9 +1,36 @@
-const sayHi = () => {
-    console.log('hello world!');
-};
+import * as d3 from 'd3';
+import * as scale from 'd3-scale';
 
-export function add(a, b) {
-    return a + b;
-}
+d3.json("/data/buses.json", function(error, json) {
+    if(error) { console.log(error.toString()); }
+    
+    // d3.select("body")
+    //   .selectAll("p")
+    //   .data(json)
+    //   .enter()
+    //   .append("p")
+    //   .text(b => { return JSON.stringify(b.location.point.geometry.coordinates[1]); } );
 
-sayHi();
+    // var svg = d3.select("body")
+    //             .append("svg")
+    //             .attr("width", 1262)
+    //             .attr("height", 800);
+    
+    // var xScale =scale.scaleLinear()
+    //                     .domain([-82.75,-83.25])
+    //                     .range([1, 1000]);
+    // var yScale =scale.scaleLinear()
+    //                     .domain([39.7, 40.25])
+    //                     .range([1, 700]);
+
+    // svg.selectAll("circle")
+    //    .data(json)
+    //    .enter()
+    //    .append("circle")
+    //    .attr("cx", function(d) { return xScale(d.location.point.geometry.coordinates[0]) })
+    //    .attr("cy", function(d) { return yScale(d.location.point.geometry.coordinates[1])  })
+    //    .attr("r", 10)
+    //    .attr("fill", "red")
+
+    
+}); 
